73af9f460feb0c1f365aacc1b78ec50612a36087
white <white@example.com>
2023-11-16 15:44:48 +0500

    greet white

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello");
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+
+        static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
+    }
+}
