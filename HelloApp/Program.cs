ba659fb089b124fec5589e822ccd6dac0313e46f
black <black@example.com>
2023-11-16 16:08:20 +0500

    hello everyone

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-
-namespace HelloApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hello");
-        }
-
-        static void GreetWhite()
-        {
-            Console.WriteLine("hello white");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("hello");
